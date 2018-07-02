74331b49d447ad695671d3e99aba65de561fc40c
Davina Leong <davina.leong.sy@gmail.com>
2018-07-02 09:46:25 +0800

    Retrieve all tasks from DB

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,20 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const mongojs = require('mongojs');
+const db = mongojs('mongodb://DavinaLeong:EagleSoar12.ml@ds121311.mlab.com:21311/learn-mean-davina',
+    ['tasks']);
 
+/**
+ * Retrieve all tasks.
+ */
 router.get('/tasks', (req, res, next) => {
-    res.send('TASK API');
+    db.tasks.find((err, tasks) => {
+        if(err) {
+            res.send(err);
+        }
+        res.json(tasks);
+    });
 });
 
 module.exports = router;
